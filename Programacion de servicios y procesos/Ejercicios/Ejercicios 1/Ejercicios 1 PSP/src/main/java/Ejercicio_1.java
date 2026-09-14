9538709d4398ac9114518d0ebeaa3b9dc3e581e4
Davidborja731 <dborjam@iesch.org>
2026-09-14 23:39:22 +0200

    2026-09-14

---
+++ Programacion de servicios y procesos/Ejercicios/Ejercicios 1/Ejercicios 1 PSP/src/main/java/Ejercicio_1.java
@@ -0,0 +1,8 @@
+public class Ejercicio_1 {
+    static void main() {
+        System.out.println("Ejercicio 1");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
